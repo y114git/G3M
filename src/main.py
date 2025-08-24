1e97f6b1ddf5d1ebfa10b1eb0c220e1d4a6ebf56
Y114 <y114mail@gmail.com>
2025-08-24 08:09:45 +0200

    module fix

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
 import sys
 import os
-project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0, project_root)
+
+if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+    path_to_src = os.path.join(getattr(sys, '_MEIPASS'), 'src')
+else:
+    path_to_src = os.path.dirname(os.path.abspath(__file__))
+
+sys.path.insert(0, path_to_src)
+
 if __name__ == '__main__':
-    from src.core.startup import run_app
-    from src.utils.file_utils import cleanup_old_updater_files
+    from core.startup import run_app
+    from utils.file_utils import cleanup_old_updater_files
     cleanup_old_updater_files()
     run_app()
