a6ce43c1ab4620bbbca9e969bbf1f683b8e53359
thej01 <152479044+thej01@users.noreply.github.com>
2025-08-29 12:22:51 -0700

    kill it
    Signed-off-by: thej01 <152479044+thej01@users.noreply.github.com>

--- src/utils/network_utils.py
+++ src/utils/network_utils.py
@@ -7,13 +7,3 @@ def check_internet_connection() -> bool:
         return True
     except requests.RequestException:
         return False
-
-def increment_launch_counter() -> None:
-    from config.constants import CLOUD_FUNCTIONS_BASE_URL
-    os_map = {'Windows': 'windows', 'Linux': 'linux', 'Darwin': 'macos'}
-    os_key = os_map.get(platform.system(), 'other')
-    try:
-        url = f'{CLOUD_FUNCTIONS_BASE_URL}/incrementLaunches'
-        requests.post(url, json={'os': os_key}, timeout=5)
-    except requests.RequestException:
-        pass
