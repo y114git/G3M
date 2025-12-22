2ea1bbc4757c7273d76331677f6cfcb7e0f1a8da
Y114 <69390801+y114git@users.noreply.github.com>
2025-12-22 18:55:39 +0100

    2.4.1
    Signed-off-by: Y114 <69390801+y114git@users.noreply.github.com>

--- src/config/constants.py
+++ src/config/constants.py
@@ -1,6 +1,6 @@
 import platform
 from .loader import get_config_value
-LAUNCHER_VERSION = '2.4.0stable'
+LAUNCHER_VERSION = '2.4.1stable'
 APP_ID = 'deltahub.y.114'
 DATA_FIREBASE_URL = get_config_value('DATA_FIREBASE_URL', '')
 CLOUD_FUNCTIONS_BASE_URL = get_config_value('CLOUD_FUNCTIONS_BASE_URL', '')
